12f5e001db4fc9f3b390d1570d53b0cb0fc3a589
Adron Hall <adron_hall@homedepot.com>
2016-03-02 12:32:29 -0800

    Removing Docker commands from main script.
    Removing and isolating the Docker commands to a different script.

--- starter/setup-osx.sh
+++ starter/setup-osx.sh
@@ -79,10 +79,4 @@ docker-machine env default;
 eval $(docker-machine env default);
 
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Shapes.jpg"';
-echo "Pull Docker image for Zookeeper & Exhibitor.";
-echo "  Docker Hub Image => https://hub.docker.com/r/adron/systemic-dockzoo/";
-echo "  Github Repo => https://github.com/Adron/systemic-dockzoo";
-docker pull adron/systemic-dockzoo;
-echo "Starting Docker image as container with ports 8181 for Exhibitor. Navigate to http://192.168.x.y:8181" \
-  " to view Exhibitor. Once image is running you can also open Kitematic to view.";
-docker run -p 8181:8181 -p 2181:2181 -p 2888:2888 -p 3888:3888 -d adron/systemic-dockzoo;
+
