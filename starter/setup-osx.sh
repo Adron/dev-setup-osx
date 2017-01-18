07533f832055b7f9e8a46a1af73c95433717c37b
Adron Hall <adronhall@gmail.com>
2017-01-18 11:30:58 -0800

    Setting up a branch with the latest testing.

--- starter/setup-osx.sh
+++ starter/setup-osx.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Antelope Canyon.jpg"';
+echo "Setting up ~/.bash_profile...";
+touch ~/.bash_profile;
+
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Color Burst 2.jpg"';
+
 echo "Showing all files, because you'll need that.";
 defaults write com.apple.finder AppleShowAllFiles YES;
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true;
+
 echo "Removing junk icons from the dock.";
 defaults write com.apple.dock persistent-apps -array;
+
 echo "Dock tile size set to 36.";
 defaults write com.apple.dock tilesize -int 36;
+
 echo "Dock set to auto-hide.";
 defaults write com.apple.dock autohide -bool true;
+
 echo "Removing dock show delay.";
 defaults write com.apple.dock autohide-delay -float 0;
 defaults write com.apple.dock autohide-time-modifier -float 0;
@@ -18,47 +26,106 @@ echo "Killing dock and finder so they'll restart."
 killall Dock 2>/dev/null;
 killall Finder 2>/dev/null;
 
-echo "Setting up ~/.bash_profile...";
-touch ~/.bash_profile;
+################################################################################
+# Install homebrew and git if they dont' exist
+################################################################################
+function install_package_managers(){
+	#install or update homebrew
+	which -s brew
+	if [[ $? != 0 ]] ; then
+	    # Install Homebrew
+			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
+	else
+	    brew update
+	fi
 
-echo "Installing Homebrew, the missing package manager for OS X!"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	#install or update git
+	if brew list -1 | grep -q "^git\$"; then
+		brew upgrade git
+	else
+		brew install git
+	fi
+}
 
-echo "Using brew to install git, python, and wget."
-brew install \
-  caskroom/cask/brew-cask \
-  git \
-  python \
-  wget;
-  
-brew tap caskroom/versions;
-echo "Using brew cask to install dropbox, firefox, chrome, java, sublime, vagrant & manager, " \
-  "virtualbox, webstorm, intellij-idea, atom, and iterm2.";
-brew cask install \
-  dropbox \
-  firefox \
-  google-chrome \
-  java \
-  virtualbox \
-  vmware-fusion \
-  webstorm \
-  intellij-idea \
-  atom \
-  visual-studio-code
-  iterm2;
+################################################################################
+# Create an SSH key for GitHub
+################################################################################
+function create_github_ssh(){
+	#Check if github SSH needs to be setup
+	pbcopy < ~/.ssh/id_rsa.pub
+	if [ $? -eq 1 ]
+	then
 
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Pink Lotus Flower.jpg"';
-echo "Installing Node Version Manager (nvm) and install latest stable version and 4.3.1.";
-echo "  Referenced from http://compositecode.com/docs/the-latest-best-way-to-install-node-js/";
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash;
-# Source bash file to enable nvm use.
-source ~/.bash_profile
-echo "Installing v4.3.1 version as fallback and stable version (likely a 5.x).";
-nvm install 4.3.1;
-nvm install stable;
-# Set the stable version as default.
-echo "  Setting stable Node.js version to default. To determine version type 'node --version'";
-nvm alias default stable;
+		#use an empty paraphrase to silently create an key pair
+		echo -e  'y\n'|ssh-keygen -q -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
+		eval "$(ssh-agent -s)"
+		ssh-add ~/.ssh/id_rsa
+		pbcopy < ~/.ssh/id_rsa.pub
 
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Shapes.jpg"';
+		# directions of what to do on GitHub.com
+		echo ""
+		echo "-------------------------------------------------------------------------------"
+		echo ""
+		echo "Your public key has been copied to the clipboard. Next a browser will navigate"
+		echo "to your github SSH Key Settings page. There may be a login gate first."
+		echo "Create a 'New SSH key' with the contents of the clipboard when you get there."
+		echo ""
+		echo "-------------------------------------------------------------------------------"
+		echo ""
+		read -n 1 -s -p "Press any key to open GitHub SSH Key Settings"
+		echo ""
+		echo ""
+		open "https://github.com/settings/keys"
+		echo ""
+		echo ""
+		read -n 1 -s -p "SSH Key created on GitHub? Press any key to continue"
+	fi
+}
 
+echo "All done!"
+
+
+#echo "Setting up ~/.bash_profile...";
+#touch ~/.bash_profile;
+#
+#echo "Installing Homebrew, the missing package manager for OS X!"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#
+#echo "Using brew to install git, python, and wget."
+#brew install \
+#  caskroom/cask/brew-cask \
+#  git \
+#  python \
+#  wget;
+#
+#brew tap caskroom/versions;
+#echo "Using brew cask to install dropbox, firefox, chrome, java, sublime, vagrant & manager, " \
+#  "virtualbox, webstorm, intellij-idea, atom, and iterm2.";
+#brew cask install \
+#  dropbox \
+#  firefox \
+#  google-chrome \
+#  java \
+#  virtualbox \
+#  vmware-fusion \
+#  webstorm \
+#  intellij-idea \
+#  atom \
+#  visual-studio-code
+#  iterm2;
+#
+#osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Pink Lotus Flower.jpg"';
+#echo "Installing Node Version Manager (nvm) and install latest stable version and 4.3.1.";
+#echo "  Referenced from http://compositecode.com/docs/the-latest-best-way-to-install-node-js/";
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash;
+## Source bash file to enable nvm use.
+#source ~/.bash_profile
+#echo "Installing v4.3.1 version as fallback and stable version (likely a 5.x).";
+#nvm install 4.3.1;
+#nvm install stable;
+## Set the stable version as default.
+#echo "  Setting stable Node.js version to default. To determine version type 'node --version'";
+#nvm alias default stable;
+#
+#osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Shapes.jpg"';
+#
